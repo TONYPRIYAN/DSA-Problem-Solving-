52a2910d1906fb4c2b05a265761ba778022c24c8
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-05-23 16:54:01 +0530

    Time: 482 ms (63.45%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,3 +1,3 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 
-Select name from Customer where referee_id != 2 or referee_id is null;
+select name from Customer where referee_id != 2 or referee_id is null;
