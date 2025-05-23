9a338ef93b43725d1c47aed0454d3dd74916a6fe
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-05-23 10:27:38 +0530

    Time: 13 ms (32.42%) | Memory: 44.7 MB (44.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -6,13 +6,29 @@ class Solution {
             return false;
         }
 
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        for(char c : s.toCharArray())
+        {
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
 
-        return Arrays.equals(arr1,arr2);
+        for(char c : t.toCharArray())
+        {
+            map2.put(c,map2.getOrDefault(c,0)+1);
+        }
+
+        for(char c : map.keySet())
+        {
+            if(!map2.containsKey(c) || map2.get(c).intValue() != map.get(c).intValue())
+            {
+                return false;
+            }
+            
+        }
+
+        return true;
 
 
     }
