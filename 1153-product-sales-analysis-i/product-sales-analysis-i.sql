a6f6d61effeee254dacf8a6fb53a716e93559d40
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-05-24 15:07:10 +0530

    Time: 1107 ms (98.76%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,6 +1,6 @@
-/* Write your PL/SQL query statement below */
+# Write your MySQL query statement below
 
 select Product.product_name,Sales.year,Sales.price from Sales
-Join
-Product on
+join
+Product on 
 Sales.product_id = Product.product_id;
