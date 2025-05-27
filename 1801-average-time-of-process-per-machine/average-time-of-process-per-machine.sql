2d855846e0d0845bab3cfb61078c082c3d08ecf5
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-05-27 12:26:08 +0530

    Time: 422 ms (20.15%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,8 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
-select a1.machine_id, Round(Avg(a2.timestamp - a1.timestamp),3) as processing_time
-from Activity a1 join Activity a2
-on a1.process_id = a2.process_id
-where a1.machine_id = a2.machine_id and
-a1.timestamp < a2.timestamp
-group by a1.machine_id
+select a1.machine_id,round(avg(a2.timestamp - a1.timestamp),3) as processing_time
+from Activity a1,Activity a2
+where a1.process_id = a2.process_id
+and a1.machine_id = a2.machine_id
+and a1.timestamp < a2.timestamp
+group by a1.machine_id;
