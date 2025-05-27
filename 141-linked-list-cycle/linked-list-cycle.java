3326ea4901f5fe64183968cffcf21fa9d7932bc4
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-05-27 10:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (63.29%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -13,20 +13,18 @@ public class Solution {
     public boolean hasCycle(ListNode head) 
     {
 
-        ListNode s = head;
         ListNode f = head;
+        ListNode s = head;
 
-    //Tortoise Hare Method
         while(f != null && f.next != null)
         {
             s = s.next;
             f = f.next.next;
-            
+
             if(f == s)
             {
                 return true;
             }
-            
         }
 
         return false;
