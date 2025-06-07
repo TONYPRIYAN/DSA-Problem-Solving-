8b1e2dc4086e36cb1fae1cbdf09b68b2507b6412
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-06-07 12:18:07 +0530

    Time: 497 ms (79.31%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,5 +1,4 @@
-# Write your MySQL query statement below
-
+/* Write your PL/SQL query statement below */
 select project_id,round(avg(experience_years),2) as average_years
 from Project join Employee
 on Project.employee_id = Employee.employee_id
