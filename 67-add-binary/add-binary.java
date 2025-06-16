8227d779d366676ecf68e09a81b25c691c5f7222
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-06-16 10:59:37 +0530

    Time: 1 ms (99.76%) | Memory: 42.3 MB (65.93%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) 
+    {
+        int i = a.length() -1;
+        int j = b.length() -1;
+        int carry = 0;
+
+        StringBuilder res = new StringBuilder();
+
+        while(i >=0 || j >= 0 || carry >0)
+        {
+            int n1 = (i >= 0) ? a.charAt(i--) - '0' : 0;
+            int n2 = (j >= 0) ? b.charAt(j--) - '0' : 0;
+
+            int sum = n1 + n2 + carry;
+
+            res.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return res.reverse().toString();
+        
+    }
+}
