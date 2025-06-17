37ff841f6a9b25356cefcf4a9865abdb578be8da
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-06-17 10:57:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (47.76%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,7 +1,8 @@
 class Solution {
     public int[] plusOne(int[] digits) 
     {
-        for(int i = digits.length-1;i>=0;i--)
+
+        for(int i=digits.length-1;i>=0;i--)
         {
             if(digits[i] < 9)
             {
@@ -11,7 +12,7 @@ class Solution {
             digits[i] = 0;
         }
 
-        digits = new int[digits.length + 1];
+        digits = new int[digits.length+1];
         digits[0] = 1;
         return digits;
         
