669f107cedcdce3c4c1bc8419de375c8c62e0a28
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-02 18:57:37 +0530

    Time: 429 ms (63.95%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 select e1.name as Employee
-from Employee as e1,Employee as e2
+from Employee e1,Employee e2
 where e1.ManagerId = e2.id and e1.salary > e2.salary;
