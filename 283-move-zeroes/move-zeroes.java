d267bac54d9e95f4316a7e625a8b6938caf4a2c1
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-07 19:41:48 +0530

    Time: 3 ms (21.86%) | Memory: 46.3 MB (22.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void moveZeroes(int[] nums) 
     {
-        int k = 0;
+        int k=0;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i] == 0)
@@ -13,9 +13,8 @@ class Solution {
                 int m = nums[i];
                 nums[i] = 0;
                 nums[i-k] = m;
-
             }
         }
-        
+
     }
 }
