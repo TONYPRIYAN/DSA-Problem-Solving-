3c21fb894c9a68d2b72b20b0c555ddecfe207a59
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-14 12:33:46 +0530

    Time: 6 ms (64.99%) | Memory: 45.7 MB (11.21%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,55 +1,47 @@
 class Solution {
-    public int[] asteroidCollision(int[] arr)
+    public int[] asteroidCollision(int[] asteroids) 
     {
         Stack<Integer> st = new Stack<>();
 
-        for(int x : arr)
+        for(int x : asteroids)
         {
-            boolean des = false;
-            while(!st.isEmpty() && x<0 && st.peek()>0)
+        boolean des = false;
+        while(!st.isEmpty() && x<0 && st.peek() > 0)
+        {
+            
+            if(st.peek() < -x)
             {
-                if(st.peek() < -x)
-                {
-                    st.pop();
-                    
-                }
-                else if(st.peek() == -x)
-                {
-                    st.pop();
-                    des = true;
-                    break;
-                }
-                else
-                {
-                    des = true;
-                    break;
-                }
-
+                st.pop();
             }
-
-            if(!des)
+            else if(st.peek() == -x)
             {
-                st.push(x);
+                st.pop();
+                des = true;
+                break;
+                
+            }
+            else
+            {
+                des  = true;
+                break;
             }
         }
 
-        
-        
-                
-    int[] arr2 = new int[st.size()];
+        if(!des)
+        {
+            st.push(x);
+        }
 
-    for(int i=0;i<st.size();i++)
-    {
-        arr2[i] = st.get(i);
-    }
 
-    
+        }
 
-            
-        
+        int[] arr = new int[st.size()];
+        for(int i=0;i<st.size();i++)
+        {
+            arr[i] = st.get(i);
+        }
 
-        
-        return arr2;
+        return arr;
         
     }
 }
