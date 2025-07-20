6aa5bf4a5e5842c3573e21777256296f2d228b3a
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-07-20 18:48:37 +0530

    Time: 532 ms (29.54%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 
 SELECT Person.FirstName, Person.LastName, Address.City, Address.State from Person LEFT JOIN Address on Person.PersonId = Address.PersonId;
