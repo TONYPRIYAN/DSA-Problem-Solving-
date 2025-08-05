73d78a29dd2c8151c65fb72be27db955c90c4916
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-05 21:03:06 +0530

    Time: 422 ms (32.34%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -1,4 +1,5 @@
-# Write your MySQL query statement below
-select class from Courses 
+/* Write your PL/SQL query statement below */
+
+select class from Courses
         group by class
         having count(student) >= 5;
