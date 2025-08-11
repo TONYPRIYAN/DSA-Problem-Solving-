7c7642d1a5b03fbcee5d8eadf9ce04c5f8ce7377
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-11 08:17:23 +0530

    Time: 3 ms (92.00%) | Memory: 43.5 MB (61.19%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,34 +1,48 @@
 class Solution {
     public boolean isAnagram(String s, String t) 
     {
-        if(s.length() != t.length())
+        // if(s.length() != t.length())
+        // {
+        //     return false;
+        // }
+
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // HashMap<Character,Integer> map2 = new HashMap<>();
+
+        // for(char c : s.toCharArray())
+        // {
+        //     map.put(c,map.getOrDefault(c,0)+1);
+        // }
+
+        // for(char c : t.toCharArray())
+        // {
+        //     map2.put(c,map2.getOrDefault(c,0)+1);
+        // }
+
+        // for(char c : map.keySet())
+        // {
+        //     if(!map2.containsKey(c) || map2.get(c).intValue() != map.get(c).intValue())
+        //     {
+        //         return false;
+        //     }
+            
+        // }
+
+        // return true;
+
+         if(s.length() != t.length())
         {
             return false;
         }
 
-        HashMap<Character,Integer> map = new HashMap<>();
-        HashMap<Character,Integer> map2 = new HashMap<>();
+        int[] freq = new int[26];
 
-        for(char c : s.toCharArray())
-        {
-            map.put(c,map.getOrDefault(c,0)+1);
-        }
-
-        for(char c : t.toCharArray())
-        {
-            map2.put(c,map2.getOrDefault(c,0)+1);
-        }
-
-        for(char c : map.keySet())
-        {
-            if(!map2.containsKey(c) || map2.get(c).intValue() != map.get(c).intValue())
-            {
-                return false;
-            }
-            
-        }
+        for(char c : s.toCharArray()) freq[c -'a']++;
+        for(char c : t.toCharArray()) if(--freq[c-'a'] < 0) return false;
 
         return true;
+
+
 
 
     }
