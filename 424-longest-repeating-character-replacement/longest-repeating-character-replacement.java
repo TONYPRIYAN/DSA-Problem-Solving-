41a686a12bea13d4147bed098e6f1ab3885d5b86
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-18 08:43:40 +0530

    Time: 9 ms (71.59%) | Memory: 44.6 MB (83.26%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -3,23 +3,22 @@ class Solution {
     {
 
         int maxlen = 0;
-        
         int l = 0;
-        int max=  0;
-       int arr[] = new int[26];
+        int[] arr = new int[26];
+        int max = 0;
 
-        for(int r=0;r<s.length();r++)
+        for(int r = 0;r< s.length();r++)
         {
-           max  = Math.max(max,++arr[s.charAt(r)-'A']);
-           if(r - l + 1 - max  > k)
-           {
-            arr[s.charAt(l)-'A']--;
-            l++;
-           }
+            max = Math.max(max,++arr[s.charAt(r) - 'A']);
 
-           maxlen = Math.max(maxlen,r-l+1);
-           
-          
+            if(r-l+1-max > k)
+            {
+                arr[s.charAt(l)-'A']--;
+                l++;
+            }
+
+            maxlen = Math.max(maxlen,r-l+1);
+
         }
 
         return maxlen;
