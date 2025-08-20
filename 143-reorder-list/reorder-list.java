7d3ef36a198a8b54587bc837b9a184113e1e9fb2
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-08-20 08:31:02 +0530

    Time: 2 ms (85.37%) | Memory: 48 MB (82.62%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -8,12 +8,13 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution 
-{
-    public static ListNode middle(ListNode head)
+class Solution {
+
+    public ListNode middle(ListNode node)
     {
-        ListNode f = head;
-        ListNode s = head;
+
+        ListNode f = node;
+        ListNode s = node;
 
         while(f != null && f.next != null)
         {
@@ -24,17 +25,22 @@ class Solution
         return s;
 
     }
+
+
     public void reorderList(ListNode head) 
     {
 
+        //Finding mid
         ListNode mid = middle(head);
 
-       
         ListNode head2 = mid.next;
         mid.next = null;
 
+        //Reverse the second part
+
         ListNode r2 = head2;
         ListNode r2prev = null;
+
         while(r2 != null)
         {
             ListNode r2next = r2.next;
@@ -45,24 +51,21 @@ class Solution
 
         head2 = r2prev;
 
-        ListNode first = head;
+        ListNode first  = head;
         ListNode second = head2;
 
         while(second != null)
         {
+            ListNode tmp1 = first.next;
+            ListNode tmp2 = second.next;
 
-            ListNode temp1 = first.next;
-            ListNode temp2 = second.next;
-
-
-            
             first.next = second;
-            second.next = temp1;
+            second.next = tmp1;
 
-            first = temp1;
-            second = temp2;
+            first = tmp1;
+            second = tmp2;
         }
-        
+
         
     }
 }
