ec2880229ea51deaabce2fcc24a3eab6213a8826
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-09-10 11:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (39.46%) - LeetSync

---
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public int maxDepth(Node root) 
+    {
+      return traverse(root);  
+    }
+
+    public int traverse(Node node)
+    {
+        if(node == null)
+            return 0;
+        int max = 0;
+        for(Node n : node.children)
+        {
+            max = Math.max(max,traverse(n));
+        }
+
+        return 1+ max;
+
+    }
+}
