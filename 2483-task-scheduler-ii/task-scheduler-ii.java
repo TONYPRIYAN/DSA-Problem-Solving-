436134ce31a2680028698414c988d451a4ce1ee4
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-09-22 07:45:52 +0530

    Time: 43 ms (26.73%) | Memory: 65.7 MB (14.75%) - LeetSync

---
+++ 2483-task-scheduler-ii/task-scheduler-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long taskSchedulerII(int[] tasks, int space) 
+    {
+
+        Map<Integer,Long> map = new HashMap<>();
+
+        long day = 0;
+
+        for(int task : tasks)
+        {
+            day++;
+
+            if(map.containsKey(task) && day - map.get(task) <= space)
+            {
+                day = map.get(task)+space+1;
+            }
+
+            map.put(task,day);
+        }
+
+        return day;
+        
+    }
+}
