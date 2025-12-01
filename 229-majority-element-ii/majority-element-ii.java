7e08b389ea0166f48e53438af0e65eea5d98d72d
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-12-01 11:38:05 +0530

    Time: 2 ms (99.81%) | Memory: 52.6 MB (14.34%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,25 +2,57 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) 
     {
 
-        HashMap<Integer,Integer> map = new HashMap<>();
-        List<Integer> list =  new ArrayList<>();
-
         int n = nums.length;
+        int cnt = 0;
+        int ele = 0;
+        int cnt2 = 0;
+        int ele2 = 0;
+
+
+        List<Integer> list = new ArrayList<>();
 
         for(int x : nums)
         {
-            map.put(x,map.getOrDefault(x,0)+1);
-        }
-
-        for(int x : map.keySet())
-        {
-            if(map.get(x) > n/3)
+            
+            if(ele == x)
             {
-                list.add(x);
+                cnt++;
+            }
+            else if(ele2 == x)
+            {
+                cnt2++;
+            }
+            else if(cnt == 0)
+            {
+                cnt = 1;
+                ele = x;
+            }
+            else if(cnt2 == 0)
+            {
+                cnt2 = 1;
+                ele2 = x;
+            }
+            else
+            {
+                cnt--;
+                cnt2--;
             }
         }
 
+
+        cnt = 0;
+        cnt2 = 0;
+        for(int x : nums)
+        {
+            if(x == ele) cnt++;
+            if(x == ele2) cnt2 ++;
+        }
+
+        if(cnt > n/3) list.add(ele);
+        if(cnt2 > n/3 && ele != ele2) list.add(ele2);
+
+
         return list;
-        
+
     }
 }
