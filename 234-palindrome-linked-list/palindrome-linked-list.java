2d669931b71caf02fb33b7e2b312d19a107c4bb7
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2025-12-17 10:54:25 +0530

    Time: 7 ms (36.38%) | Memory: 101.2 MB (13.77%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,66 +8,34 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution 
-{
-    public ListNode reverse(ListNode slow)
-    {
-
-        
-        ListNode current = slow;
-        ListNode prev = null;
-
-        while(current != null)
-        {
-            ListNode tmp = current.next;
-            current.next = prev;
-            prev = current;
-            current = tmp;
-
-            
-        }
-
-        return prev;
-
-
-
-    }
-
-
-
+class Solution {
     public boolean isPalindrome(ListNode head) 
     {
 
-        ListNode s = head;
-        ListNode f = head;
+        ArrayList<Integer> arr = new ArrayList<>();
 
-        if(head == null || head.next == null)
+        ListNode tmp = head;
+
+        while(tmp != null)
         {
-            return true;
+            arr.add(tmp.val);
+            tmp = tmp.next;
         }
 
-        while(f != null && f.next != null)
+        int l = 0;
+        int r = arr.size() - 1;
+
+        while(l<=r)
         {
-            s = s.next;
-            f = f.next.next;
-        }
-
-        s= reverse(s);
-        
-        ListNode s1 = head;
-
-        
-
-        while(s != null)
-        {
-            if(s1.val != s.val)
+            if(arr.get(l) != arr.get(r))
             {
                 return false;
             }
-            s1 = s1.next;
-            s = s.next;
-            
+            l++;
+            r--;
         }
+
         return true;
+        
     }
 }
