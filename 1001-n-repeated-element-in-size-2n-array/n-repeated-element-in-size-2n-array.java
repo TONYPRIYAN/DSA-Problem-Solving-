4b52bc90e3f7437c91da744c14ebf8ba2799e3ce
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-01-02 10:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.4 MB (50.87%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -1,11 +1,15 @@
 class Solution {
-    public int repeatedNTimes(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int x : nums) {
-            if (!set.add(x)) {
-                return x;
-            }
+    public int repeatedNTimes(int[] nums) 
+    {
+
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(nums[i] == nums[i+1]) return nums[i];
+            if(i+2 < nums.length && nums[i] == nums[i+2]) return nums[i];
+            if(i+3 < nums.length && nums[i] == nums[i+3]) return nums[i];
+
         }
         return -1;
+        
     }
 }
