616bffd02bc054042f62bb1511b78b406d23fdfd
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-02-20 08:52:00 +0530

    Time: 6 ms (87.24%) | Memory: 43.8 MB (94.61%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+
+        if (s1.length() > s2.length()) return false;
+
+        int[] count = new int[26];
+
+        
+        for (char c : s1.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+
+           
+            count[s2.charAt(right) - 'a']--;
+
+            
+            if (right - left + 1 > s1.length()) {
+                count[s2.charAt(left) - 'a']++;
+                left++;
+            }
+
+            
+            if (allZero(count)) return true;
+        }
+
+        return false;
+    }
+
+    private boolean allZero(int[] arr) {
+        for (int x : arr) {
+            if (x != 0) return false;
+        }
+        return true;
+    }
+}
