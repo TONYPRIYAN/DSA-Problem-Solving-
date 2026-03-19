5b016db3cec57321feb641fd3435b49a98ab7429
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-03-19 12:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (82.41%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -14,11 +14,8 @@
  * }
  */
 class Solution {
-    
-    
     public boolean isSameTree(TreeNode p, TreeNode q) 
     {
-        
         if(p == null && q == null)
         {
             return true;
@@ -31,13 +28,9 @@ class Solution {
         {
             return false;
         }
-
-
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
         
+
+    return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+
     }
-
-    
-
-
 }
