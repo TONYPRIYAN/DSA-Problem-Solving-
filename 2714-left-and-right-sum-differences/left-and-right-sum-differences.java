462e30b72659c642b76916aeb0ce753fe9a1d416
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-06-06 08:10:03 +0530

    Time: 2 ms (98.84%) | Memory: 46.4 MB (84.77%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] leftRightDifference(int[] nums) 
+    {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int sum = 0;
+
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = sum;
+            sum += nums[i];
+        }
+
+        sum =0;
+
+        for(int i=n-1;i>=0;i--)
+        {
+            ans[i] = Math.abs(ans[i] - sum);
+            sum += nums[i];
+        }
+
+        return ans;
+
+
+
+
+        
+    }
+}
