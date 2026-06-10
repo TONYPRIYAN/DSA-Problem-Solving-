c4c831e47ffb734f53bbf5249d395e6396e8db76
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-06-10 08:34:04 +0530

    Time: 1 ms (98.42%) | Memory: 47.6 MB (87.11%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,20 +1,26 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) 
     {
+
         int len = flowerbed.length;
 
-        for(int i=0;i<len && n>0;i++)
+        for(int i=0;i<len && n > 0;i++)
         {
-            if(flowerbed[i] == 0){
-            boolean left = (i == 0) || (flowerbed[i-1]==0);
-            boolean right = (i == len-1) || (flowerbed[i+1]==0);
-
-            if(left && right)
+            if(flowerbed[i] == 0)
             {
-                flowerbed[i] = 1;
-                n--;
+
+                boolean left = (i == 0) || (flowerbed[i-1] == 0);
+                boolean right = (len-1 == i) || (flowerbed[i+1] == 0);
+
+
+                if(left && right)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+
             }
-            }
+          
         }
 
         return n == 0;
