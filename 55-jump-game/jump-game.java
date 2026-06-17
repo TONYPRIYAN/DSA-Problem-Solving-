64470e57a0bfb353d07ca5a5e0ed66faf1d02b6a
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-06-17 08:16:30 +0530

    Time: 2 ms (88.60%) | Memory: 47.9 MB (41.98%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,21 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) 
     {
+
         int l = 0;
-        
 
         for(int i=0;i<nums.length;i++)
         {
-           if(i > l)
-           {
-            return false;
-           }
+            if(l < i)
+            {
+                return false;
+            }
 
-           l = Math.max(l,i+nums[i]);
-
-           
+            l = Math.max(l,i+nums[i]);
         }
-        
+
         return true;
         
     }
