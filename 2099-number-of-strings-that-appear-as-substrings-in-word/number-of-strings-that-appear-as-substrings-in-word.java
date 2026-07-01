b4f2d0e9cab7456e9717b4508b49b0865c9024f1
SHANMUGA PRIYAN <103183712+TONYPRIYAN@users.noreply.github.com>
2026-07-01 08:41:15 +0530

    Time: 1 ms (75.05%) | Memory: 43.4 MB (61.12%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -1,4 +1,29 @@
 class Solution {
+
+    public int strStr(String haystack, String needle) 
+    {
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(n > m) return -1;
+
+        for(int i =0;i<=m-n;i++)
+        {
+            if(haystack.charAt(i) == needle.charAt(0) && haystack.charAt(i+n-1) == needle.charAt(n-1))
+            {
+                int j=0;
+                while(j < n && haystack.charAt(i+j) == needle.charAt(j))
+                {
+                    j++;
+                }
+                if(j == n) return i;
+
+            }
+        }
+
+        return -1;
+        
+    }
     public int numOfStrings(String[] patterns, String word) 
     {
 
@@ -6,7 +31,7 @@ class Solution {
 
         for(String str : patterns)
         {
-            if(word.indexOf(str) != -1)
+            if(strStr(word,str) != -1)
             {
                 tot++;
             }
